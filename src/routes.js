1326651337e70a5ab4d0014ba337e9b51b60b540
Jaagrit <jaagritgaur@yahoo.in>
2024-10-06 00:03:39 -0400

    deleted junk

--- src/routes.js
+++ src/routes.js
@@ -19,13 +19,13 @@
 import Charity from "views/Charity";
 import Dashboard from "views/Dashboard.js";
 import Notifications from "views/Notifications.js";
-import Icons from "views/Icons.js";
-import Typography from "views/Typography.js";
-import TableList from "views/Tables.js";
+/*import Icons from "views/Icons.js";*/
+/*import Typography from "views/Typography.js";*/
+/*import TableList from "views/Tables.js";*/
 import Maps from "views/Map.js";
 import UserPage from "views/User.js";
-import UpgradeToPro from "views/Upgrade.js";
-import Detect from "views/Detect";
+/*import UpgradeToPro from "views/Upgrade.js";*/
+/*import Detect from "views/Detect";*/
 
 var routes = [
   {
@@ -35,20 +35,20 @@ var routes = [
     component: <Dashboard />,
     layout: "/admin",
   },
-  {
+  /*{
     path: "/Detect",
     name: "Detect",
     icon: "nc-icon nc-camera-compact",
     component: <Detect />,
     layout: "/admin",
-  },
-  {
+  },*/
+  /*{
     path: "/icons",
     name: "Icons",
     icon: "nc-icon nc-diamond",
     component: <Icons />,
     layout: "/admin",
-  },
+  },*/
   {
     path: "/maps",
     name: "Maps",
@@ -77,27 +77,27 @@ var routes = [
     component: <Charity />,
     layout: "/admin",
   },
-  {
+  /*{
     path: "/tables",
     name: "Table List",
     icon: "nc-icon nc-tile-56",
     component: <TableList />,
     layout: "/admin",
-  },
-  {
+  },*/
+  /*{
     path: "/typography",
     name: "Typography",
     icon: "nc-icon nc-caps-small",
     component: <Typography />,
     layout: "/admin",
-  },
-  {
+  },*/
+  /*{
     pro: true,
     path: "/upgrade",
     name: "Upgrade to PRO",
     icon: "nc-icon nc-spaceship",
     component: <UpgradeToPro />,
     layout: "/admin",
-  },
+  },*/
 ];
 export default routes;
